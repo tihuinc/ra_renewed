d0e98efcf61c50fc31166b0b8bdd094381eadf3d
Hu <tianying@tianying-mac.(none)>
2012-08-12 14:19:31 -0700

    Initial import.

---
+++ README.md
@@ -0,0 +1,27 @@
+# ReadMe
+
+
+## Deployment
+
+```
+ey deploy
+```
+Remove
+## Thanks
+
+The original scaffold for this application was created by [App Scrolls](http://appscrolls.org).
+
+The project was created with the following scrolls:
+
+* active_admin
+* git
+* jquery
+* mysql
+* passenger
+* rails_admin
+* rails_basics
+* rspec
+* twitter_bootstrap
+* simple_form
+* thin
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RaRenewed::Application.config.secret_token = 'fc6017e7b7cbcc4aeb6274407fc4d6a27bdd9cc9f2a3d9f248aff88a7f492a3675dc45dba65c7ccbf246ac9e5a93f658dcfe1127b494279a998c1450d0e79186'
